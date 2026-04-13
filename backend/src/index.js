5ea7df00d01652a56a6188f20f047e1ed5c72a0e
Meekness-zw <166127215+Meekness-zw@users.noreply.github.com>
2026-04-13 11:14:46 +0200

    Delete backend directory

--- backend/src/index.js
+++
@@ -1,5 +0,0 @@
-// Optional: simple helper export if you ever want to import these services directly
-export * from './supabaseClient.js';
-export * from './authService.js';
-export * from './userService.js';
-
